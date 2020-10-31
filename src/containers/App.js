d50d0697f78f689c650e7337ccd2ad8eea1af453
TateFowble <tatefowble@gmail.com>
2020-10-31 01:14:25 -0500

    Changes to app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,9 +12,9 @@ import HomePage from '../Pages/HomePage';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HomePage /> {/*  Homepage was moved to /Pages/HomePage.js  */}
-      </div>
+        <div className="App">
+          <HomePage /> {/*  Homepage was moved to /Pages/HomePage.js  */}
+        </div>
     );
   }
 }
