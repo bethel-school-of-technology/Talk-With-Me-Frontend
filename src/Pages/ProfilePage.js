8a737dd54a3c15ebd5f9df546fc5694de806e352
Dylan Casale <Dylanacasale@gmail.com>
2020-11-09 15:46:45 -0600

    ok then

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -4,6 +4,8 @@ import React from 'react';
 const ProfilePage = () => {
     return (
         <div className='flexcontainer background fl w-100 vertical h-400'>
+
+        
         </div>
     );
 }
